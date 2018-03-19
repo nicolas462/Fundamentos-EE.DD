ef9cc623e5704895cbe03ea31baa7d3989401dc6
nicolas462 <37225697+nicolas462@users.noreply.github.com>
2018-03-19 17:52:41 -0500

    Update Recursiones.py

--- Recursiones.py
+++ Recursiones.py
@@ -1,7 +1,3 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
-
 __author__ = "Nicolás Espejo"
 __date__ = "$19/03/2018 05:27:24 PM$"
 
