d0472afd2ee6bdc73dabb93a339c7d86f0ab8ba1
Nicolás Espejo <nespejobern@uniminuto.edu.co>
2018-03-22 10:24:23 -0500

    Comentarios.

--- Parcial_corte1/src/Cubos.java
+++ Parcial_corte1/src/Cubos.java
@@ -13,17 +13,17 @@ class Cubos {
 			BufferedReader br = new BufferedReader(new InputStreamReader ( System.in ));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter ( System.out ));
 			int a, b, c, result;
-			int cases = Integer.parseInt(br.readLine());
+			int cases = Integer.parseInt(br.readLine()); //Lee la cantidad de casos y los pasa a entero.
 			for(int i =0; i < cases; i++) {
-				String line = br.readLine();
-				String [] datos = line.split(" ");
+				String line = br.readLine(); //Lee una línea de String.
+				String [] datos = line.split(" ");//Divide la línea anterior por cada espacio y la asigna al vector.
 				a = Integer.parseInt (datos[0]);
 				b = Integer.parseInt (datos[1]);
 				c = Integer.parseInt (datos[2]);
-				int md = mcd(a, b);
-				md = mcd(md, c);
-				result = (a/md)*(b/md)*(c/md);
-				bw.write(md + " " + result + "\n");
+				int md = mcd(a, b); // Se halla el MCD de a y b.
+				md = mcd(md, c); //Se halla el MCD del resultado de a y b, con c.
+				result = (a/md)*(b/md)*(c/md); //Cantidad de cubos resultante.
+				bw.write(md + " " + result + "\n"); //Se imprime el resultado.
 			}
 			bw.flush();
 			bw.close();
