5710cfe9ca31cd7a6eb84e75ee6f32f85b127d71
Nicolás Espejo <nespejobern@uniminuto.edu.co>
2018-03-23 07:46:01 -0500

    Ciclo.

--- Parcial_corte1/src/Monster_String.java
+++ Parcial_corte1/src/Monster_String.java
@@ -1,11 +1,14 @@
 import java.io.*;
 
 public class Monster_String {
-	public static int factorial (int n) {
-		if (n==1)
-			return 1;
-		else
-			return n* factorial(n-1);
+	public String eat() {
+		/*
+		 *		 0
+		 *    1	 2 	3
+		 *  2  3   3
+		 * 3
+		 */
+		return "";
 	}
 	public static void main(String[] args) {
 		try
@@ -13,20 +16,25 @@ public class Monster_String {
 			BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 			int t = Integer.parseInt(br.readLine()); //Cantidad de casos.
+			int j = 0;
 			for (int i = 0; i<=t; i++) {
 				int n = Integer.parseInt(br.readLine()); //Cantidad de veces que se ingresan Srings.
 				String [] k = new String[n]; //Se crea vector para almacenar los Strings.
-				for (int j = 0; j<=n ; j++) //Ciclo para las entradas del vector.
+				for (j = 0; j<=n ; j++) //Ciclo para las entradas del vector.
 				{
 					k [j] = br.readLine();
 				}
+				for (j = 0; j<=n ; j++)
+				{
+					for (int l = 0; l<=n ; l++)
+					{
+						if (k[n].equals(k[l])){
+						
+						}
+					}
+				}
 				int n1, k1;
-				n1 = n;
-				k1 = 3;
-				int nf= factorial(n1);
-				int nfk= factorial(n-k1);
-				int resultado = nf/nfk;
-				System.out.println("El resultado es: " + resultado);
+			
 			}
 			bw.flush();
 			bw.close();
