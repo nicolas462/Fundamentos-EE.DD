ec00068e36452c482503d70355ae715c124fb91a
nicolas462 <37225697+nicolas462@users.noreply.github.com>
2018-04-15 17:52:26 -0500

    Python 3.

---
+++ Proyeto corte 2/Insertion Sort.py
@@ -0,0 +1,21 @@
+""" Se ordenarán datos de un arreglo recibidos por consola
+con el algoritmo 'Insertion Sort'"""
+def insertionSort (a):
+	x = 0
+	index = 0
+	for i in range (0, len(a)):
+		#Seleccionar valor para mover
+		x = a[i] #Se guarda el valor en una variable porque después se usará.
+		index = i #Se guarda i en una variable porque se restarán valores.
+		#Localizar posición vacía para insertar valor
+		while index > 0 and a[index-1] > x :
+			a[index] = a[index - 1] #Se reemplaza el número por el número anterior a éste.
+			index -= 1
+		#Insertar el valor en la posición vacía. Se usa el valor que se guardó anteriormente.
+		a[index] = x
+
+print ("Ingrese números: ")
+a = input()
+list = a.split(",") #Se dividen los valores por comas y se guardan en una lista.
+insertionSort(list); #Se llama a la función
+print ("Arreglo ordenado :", list )
