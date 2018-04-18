fb827cbbe39bc8247632cba44002868ec88243fb
nicolas462 <37225697+nicolas462@users.noreply.github.com>
2018-04-17 23:21:42 -0500

    Interpolation Search

---
+++ Proyeto corte 2/Interpolation Search.py
@@ -0,0 +1,36 @@
+""" Buscar un valor en un arreglo con el algoritmo de 'Binary Search'"""
+
+#Algoritmo sin recursión
+def interpolationSearch(a, x):
+	lowerBound = 0
+	upperBound = len (a) -1
+	index = -1
+	part1 = (upperBound - lowerBound)
+	part2 = (int (a[upperBound]) - int (a[lowerBound]))
+	part3 = int (x) - int (a[lowerBound])
+	#Si el límite superior es menor que el inferior, no hay una solución factible. 
+	while lowerBound < upperBound:
+		middlePoint = int (lowerBound + (part1 / part2) * part3)
+		if x == a[middlePoint]:
+			#EL valor ha sido encontrado. 
+			index = middlePoint
+			break
+		elif x < a[middlePoint]:
+			upperBound = middlePoint -1
+		else:
+			lowerBound = middlePoint + 1
+	if lowerBound == upperBound and a[lowerBound] == x:
+		index = lowerBound
+	return index
+
+
+print ("Ingrese números: ")
+a = input()
+print ("Ingrese número a buscar: ")
+x = input()
+list = a.split(",") #Se dividen los valores por comas y se guardan en una lista.
+indice = interpolationSearch(list, x) #Se guarda el valor de la función sin recursión.
+if (indice >= 0):
+	print ("El número está en el índice: ", indice, "\n")
+else:
+	print ("El número no se encontró.\n")
