d7128e978582061737030c26ffc9546a687fc159
nicolas462 <37225697+nicolas462@users.noreply.github.com>
2018-04-18 18:53:58 -0500

    Cambios

--- Proyeto corte 2/Interpolation Search.py
+++ Proyeto corte 2/Interpolation Search.py
@@ -1,20 +1,17 @@
 """ Buscar un valor en un arreglo con el algoritmo de 'Interpolation Search'"""
-
-def interpolationSearch(a, x):
+def interpolationSearch (A, x):
 	lowerBound = 0
-	upperBound = len (a) -1
+	upperBound = len (A) -1
 	index = -1
-	part1 = (upperBound - lowerBound)
-	part2 = (int (a[upperBound]) - int (a[lowerBound]))
-	part3 = int (x) - int (a[lowerBound])
+	part1 = int (lowerBound + (upperBound - lowerBound)/ (A[upperBound] - A[lowerBound]))
 	#Si el límite superior es menor que el inferior, no hay una solución factible. 
 	while lowerBound < upperBound:
-		middlePoint = int (lowerBound + (part1 / part2) * part3)
-		if x == a[middlePoint]:
+		middlePoint = part1 * (x - A[lowerBound])
+		if x == A [middlePoint]:
 			#El valor ha sido encontrado. 
 			index = middlePoint
 			break
-		elif x < a[middlePoint]:
+		elif x < A[middlePoint]:
 			upperBound = middlePoint -1
 		else:
 			lowerBound = middlePoint + 1
@@ -22,14 +19,16 @@ def interpolationSearch(a, x):
 		index = lowerBound
 	return index
 
-
 print ("Ingrese números: ")
-a = input()
+line = input()
+lista = line.split (",")
+enteros = [] #Se guardarán enteros
+for i in range(0, len(lista)): #Pasar str a int
+	enteros.append(int (lista[i]))
+
 print ("Ingrese número a buscar: ")
-x = input()
-list = a.split(",") #Se dividen los valores por comas y se guardan en una lista.
-indice = interpolationSearch(list, x) #Se guarda el valor de la función.
-if (indice >= 0):
-	print ("El número está en el índice: ", indice, "\n")
+x = int (input())
+if (interpolationSearch(enteros, x) != -1):
+	print ("El número está en el índice: ", interpolationSearch(enteros, x))
 else:
-	print ("El número no se encontró.\n")
+	print ("No se encontró el número.")
