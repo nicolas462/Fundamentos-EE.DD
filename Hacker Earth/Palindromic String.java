e91e6ab97044345c23fd2a9dc2515b93c7f38d92
nicolas462 <37225697+nicolas462@users.noreply.github.com>
2018-04-18 19:48:39 -0500

    Palindromic String
    https://www.hackerearth.com/submission/16013562/

---
+++ Hacker Earth/Palindromic String.java
@@ -0,0 +1,35 @@
+import java.io.*;
+public class Clase1 {
+	public String palindromo(String s) //Revierte String
+	{
+		if (s.length()==1) {
+			return s;
+		}
+		else {
+			return palindromo (s.substring(1, s.length())) + s.charAt(0) ;
+		}
+		
+	}
+	public static void main(String[] args) {
+		try {
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter (System.out));
+			BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+			Clase1 prueba = new Clase1();//Llamar función.
+			String s = br.readLine();
+			if (prueba.palindromo(s).equals(s)) 
+			{
+				bw.write("YES");
+			}
+			else {
+				bw.write("NO");
+			}
+			bw.flush();
+			
+			
+		
+		}
+		catch (Exception ex ) {}
+ 
+	}
+ 
+}
