3a6124347dcbc10ad01c14bc3126d45d307e2bfc
nicolas462 <37225697+nicolas462@users.noreply.github.com>
2018-04-27 17:01:57 -0500

    Update and rename ASCII Value.java to Value.java

--- Hacker Earth/Value.java
+++ Hacker Earth/Value.java
@@ -1,14 +1,11 @@
 import java.io.*;
-public class Clase1 {
+public class Value {
 	public static void main(String[] args) {
 		try {
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter (System.out));
 			BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
 			char let = (char) br.read();
-			System.out.println((int) let);
-			
-			
-					
+			System.out.println((int) let);				
 		}
 		catch (Exception ex ) {}
  
