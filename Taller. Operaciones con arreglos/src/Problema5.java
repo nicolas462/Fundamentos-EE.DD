aa64f0dbb56cba9cc2901d3496e562fda85bb6c1
Nicolás Espejo <nespejobern@uniminuto.edu.co>
2018-05-01 10:25:47 -0500

    Correción

--- Taller. Operaciones con arreglos/src/Problema5.java
+++ Taller. Operaciones con arreglos/src/Problema5.java
@@ -21,12 +21,47 @@ public class Problema5 {
 	 * @param datos = Arreglo de String que se recibe.
 	 * @return = Retorna arreglo de enteros.
 	 */
-	public static int[] arreglo (String datos [])
+	public static int[] arreglo (String [] datos )
 	{
 		int [] valores = new int [datos.length]; 
 		for (int i= 0; i<datos.length; i++)
 			valores[i] = Integer.parseInt(datos[i]);
 		return valores;
+	}
+	/**
+	 * Define el patrón de la suceción aritmética.
+	 * @param datos = Vector con números.
+	 * @return = Valor faltante llamando a otra función.
+	 */
+	public static int patr (int [] datos) {
+		int patr = 0;
+		int aux = 0, aux2 = 0;
+		for (int i = datos.length - 1; i >0 ; i--) {
+			patr = datos [i] - datos [i -1];
+			//Si el valor del patrón es mayor (donde se rompe la suceción), se vuelve a establecer a el valor anterior.
+			if (aux < patr && aux > 0) {
+				aux2 = aux;
+				aux = patr;
+				patr = aux2;
+			}
+			else
+				aux = patr;
+		}
+		return faltante (datos, patr);
+	}
+	/**
+	 * Se busca dónde la suceción no cumple la diferencia del patrón.
+	 * @param arreglo = Suceción aritmética.
+	 * @param patron = patron previamente definido
+	 * @return = Número faltante en la suceción.
+	 */
+	public static int faltante (int [] arreglo, int patron) {
+		int respuesta = 0;
+		for (int i = 1; i<arreglo.length; i++) { 
+			if (arreglo [i]-arreglo[i-1] != patron) 
+				respuesta = arreglo [i-1] + patron; 	
+		}
+		return respuesta;
 	}
 	/**
 	 * @param args
@@ -37,18 +72,9 @@ public class Problema5 {
 			BufferedReader br = new BufferedReader (new InputStreamReader (System.in));
 			String n = br.readLine(); //Se lee la entrada.
 			String [] datos = n.split(","); //Se almacenan los datos de entrada en un arraglo de Strings.
-			int [] valores = arreglo(datos); //Se llama a la función para crear el arreglo de enteros.
-			
-			int respuesta =0;
-			int patr = valores[1] - valores[0]; //Se define el patrón de la secuencia.
-			for (int i = 1; i<valores.length; i++) { 
-				if (valores [i]-valores[i-1] != patr) //Se busca dónde la sucesión no cumple la diferencia del patrón.
-					respuesta = valores [i-1] + patr; //Se obtiene el número faltante.
-				
-			}
-			bw.write("Respuesta: "+respuesta);
-			bw.flush();
-			
+			int [] valores = arreglo(datos); //Se llama a la función para crear el arreglo de enteros.	
+			bw.write("Respuesta: "+ patr (valores)); //1, 5, 7, 9 //RTA: 3
+			bw.flush();	
 		}
 		catch(Exception ex) {}
 
